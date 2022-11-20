1af52ad96a80a42b7aaf9775e262ff5d939c193f
divinemuffin <guston35@gmail.com>
2022-11-20 02:26:17 +0200

    re-cached

---
+++ flutter_route/lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:albums_route/pages/not_found_page.dart';
+import 'package:albums_route/routes.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  AppRouter.setupRouter();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: AppRouter.router.generator,
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (context) => NotFoundPage());
+      },
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Routes'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Routes',
+              ),
+            ],
+          ),
+        ),
+        drawer: NavigationDrawer());
+  }
+}
+
+class NavigationDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: SafeArea(
+        child: Column(
+          children: [
+            ListTile(
+                leading: Icon(Icons.home),
+                title: Text("Home"),
+                trailing: Icon(Icons.chevron_right),
+                onTap: () {
+                  print("Navigating home");
+                  Navigator.of(context).pushNamed("/home");
+                }),
+            ListTile(
+              leading: Icon(Icons.people),
+              title: Text("Artists"),
+              trailing: Icon(Icons.chevron_right),
+              onTap: () => Navigator.of(context).pushNamed("/artists"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ flutter_route/lib/pages/artists_list_page.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+import 'package:albums_route/fetch_file.dart';
+import 'package:albums_route/main.dart';
+import 'package:flutter/material.dart';
+
+class ArtistsListPage extends StatefulWidget {
+  ArtistsListPage({Key? key}) : super(key: key);
+
+  @override
+  _ArtistsListPageState createState() => _ArtistsListPageState();
+}
+
+class _ArtistsListPageState extends State {
+  Future<String> _artistsFuture = fetchFileFromAssets("./assets/artists.json");
+
+  List<Widget> parseAndBuildList(String jsonData) {
+    final List parsedList = jsonDecode(jsonData);
+    return parsedList.map((_artist) {
+      return ListTile(
+          title: Text(_artist["name"]),
+          trailing: Icon(Icons.chevron_right),
+          onTap: () {
+            print("/artists/${_artist['link']}");
+            Navigator.of(context).pushNamed("/artists/${_artist['link']}", arguments: _artist);
+          });
+    }).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Authors List'),
+      ),
+      drawer: NavigationDrawer(),
+      body: Center(
+        child: FutureBuilder(
+          future: _artistsFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done && snapshot.data != null) {
+              final _list = parseAndBuildList(snapshot.data as String);
+              return ListView.separated(
+                itemCount: _list.length,
+                separatorBuilder: (BuildContext context, int index) => const Divider(),
+                itemBuilder: (context, index) => _list[index],
+              );
+            }
+            return Center(
+              child: Text("Loading :::"),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
